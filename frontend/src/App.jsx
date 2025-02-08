a571745038207fa9f7ddd178f66b25d145ac8562
Adarsh Gupta <csedsadarsh@gmail.com>
2025-02-09 02:23:57 +0530

    dashboard for task mange

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
 import Home from './components/Home';
 import Room from './components/Room';
 import ViewerRoom from './components/ViewerRoom';
@@ -13,10 +15,12 @@ function App() {
       <SocketProvider>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/room/:roomId" element={<Room />} />
-          <Route path="/view/:roomId" element={<ViewerRoom />} />
+          <Route path="/viewer/:roomId" element={<ViewerRoom />} />
           <Route path="/code/:roomId" element={<CodeShare />} />
           <Route path="/fileshare" element={<FileShare />} />
+          {/* <Route path='/snippet' element={<SnippetLibrary />} /> */}
         </Routes>
       </SocketProvider>
     </Router>
