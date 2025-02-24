f6ec8022312eb5b2f59057de6c1f07df4e7c6169
Adarsh Gupta <109406757+Adarsh7825@users.noreply.github.com>
2025-02-24 18:13:00 +0530

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,4 +2,4 @@ const isDevelopment = import.meta.env.VITE_NODE_ENV === 'development';
 
 export const SERVER_URL = isDevelopment 
     ? 'http://localhost:8181'
-    : 'https://screen-share-k727.onrender.com'; 
+    : 'https://devshares.onrender.com'; 
